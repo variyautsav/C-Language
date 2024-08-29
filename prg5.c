4032c895538171d39400a50b04846b0b0338c88a
variya utsav <utsavvariya02@gmail.com>
2024-08-29 17:04:37 +0530

    declaration

---
+++ prg5.c
@@ -0,0 +1,13 @@
+// declaration
+#include<stdio.h>
+int main ()
+{
+int c;
+int a=10, b=20;
+printf("\n a+b =%d", a+b);
+printf("\n %d+%d=%d",a,b,a+b);
+c=a+b;
+printf("\n %d+%d=%d",a,b,c);
+
+return 0;
+}
