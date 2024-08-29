042a060a80b4fc407dca1c50d97b662fcee8c702
variya utsav <utsavvariya02@gmail.com>
2024-08-29 16:27:10 +0530

    second

---
+++ p2_exscp_swquences.c
@@ -0,0 +1,14 @@
+// exscap sequences
+
+#include <stdio.h>
+
+int main()
+{
+ 	printf(" hello ");
+	printf(" \n hello");
+	printf("\n hello");
+	printf ("\n \t hello");
+	printf ("\n hello");
+
+	 return 0;
+}
