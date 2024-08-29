eec80716c5546dda86d9f83c7764122569a12007
variya utsav <utsavvariya02@gmail.com>
2024-08-29 16:49:08 +0530

    comments

---
+++ prg4.c
@@ -0,0 +1,12 @@
+// pogram: 4
+#include <stdio.h>
+int main()
+{
+//printf(" 1 hello");
+printf(" 2 hello");
+/*printf(" 3 hello");
+printf(" 4 hello");
+printf(" 5 hello");*/
+printf(" 6 hello");
+ return 0;
+}
