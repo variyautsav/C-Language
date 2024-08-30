5553b92e82c984158ec5fab0afaf9a0de4c912dc
variya utsav <utsavvariya02@gmail.com>
2024-08-30 16:05:52 +0530

    user input

---
+++ prg12.c
@@ -0,0 +1,26 @@
+// user input
+#include <stdio.h>
+int main()
+{
+ 	int a,b;
+	printf("entre a :");
+	scanf("%d",&a);
+	printf(" enter the b:");
+	scanf("%d", &b);
+
+
+
+	printf("\n values :a: %d \t b: %d",a,b);
+
+	printf("\n sizeof int %d" ,sizeof(int));
+	printf("\n sizeof float %d" ,sizeof(float));
+	printf("\n sizeof char %d" ,sizeof(char));
+
+	printf("\n sizeof double %d" ,sizeof(double));
+	printf("\n sizeof long double %d" , sizeof(long double));
+
+
+
+
+	return 0;
+}
