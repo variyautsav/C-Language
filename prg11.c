7abc30bf313f254176cbaa63ca26d288eb48edd8
variya utsav <utsavvariya02@gmail.com>
2024-08-30 16:05:06 +0530

    besic_opretion

---
+++ prg11.c
@@ -0,0 +1,14 @@
+// area of teingle
+
+#include <stdio.h>
+ int main()
+{
+	int b=89,h=30,area;
+	printf("enter the b");
+	scanf("%d",&b);
+	printf("enter the h");
+	scanf("%d", &h);
+	area=0.5*b*h;
+	printf("\n area of teingle %d", area);
+	return 0;
+}
