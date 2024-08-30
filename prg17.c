2a216cd17f01d1744fd09d69c722a8f5bc1ea7cf
variya utsav <utsavvariya02@gmail.com>
2024-08-30 16:12:12 +0530

    convert the   inches to cm

---
+++ prg17.c
@@ -0,0 +1,19 @@
+//convert the   inches to cm
+
+#include<stdio.h>
+
+int main()
+{
+
+	int inch;
+	float cm;
+
+	printf("enter the inch");
+	scanf("%d",&inch);
+
+	cm=inch*2.54;
+
+	printf("\n  inche to cm is:%f",cm);
+
+	return 0;
+}
