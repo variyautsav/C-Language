606e3cd9b3df70dda70d0827522ef4eecf9aabc3
variya utsav <utsavvariya02@gmail.com>
2024-08-30 16:10:29 +0530

    doller to rupee

---
+++ prg15.c
@@ -0,0 +1,17 @@
+//doller to rupee
+
+#include<stdio.h>
+
+int main()
+{
+
+	float d,r;
+
+	printf("enter the doller");
+	scanf("%f",&d);
+
+	r=d*83;
+	printf("\n ruppes of doller :%f",r);
+
+	return 0;
+}
