ee38c31a11ad68582499ef72328255c0009c725b
variya utsav <utsavvariya02@gmail.com>
2024-08-30 16:07:03 +0530

    reletional opreter

---
+++ prg13.c
@@ -0,0 +1,21 @@
+//relational operator check
+
+#include<stdio.h>
+
+int main()
+{
+
+	int a=10,b=9;
+
+	printf("\n a>b =%d",a>b);
+	printf("\n a>=b =%d",a>=b);
+
+	printf("\n a<b =%d",a<b);
+	printf("\n a<=b =%d",a<=b);
+
+	printf("\n a==b =%d",a==b);
+	printf("\n a!=b =%d",a!=b);
+
+
+	return 0;
+}
