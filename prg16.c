c4612f2c563c729b529b0b5f0c6c2426f4cafccb
variya utsav <utsavvariya02@gmail.com>
2024-08-30 16:11:37 +0530

    convert the feet into inches

---
+++ prg16.c
@@ -0,0 +1,18 @@
+//convert the feet into inches
+
+#include<stdio.h>
+
+int main()
+{
+
+	int feet,inch;
+
+	printf("enter the feet");
+	scanf("%d",&feet);
+
+	inch=feet*12;
+
+	printf("\n feet of inche is:%d",inch);
+
+	return 0;
+}
