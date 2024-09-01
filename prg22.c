ad944e99562eb2ae316ce096d838e6eed9906f38
variya utsav <utsavvariya02@gmail.com>
2024-09-01 07:02:57 +0530

    positive or nagetive number

---
+++ prg22.c
@@ -0,0 +1,25 @@
+//number is positive or nagitive
+
+#include<stdio.h>
+
+int main()
+{
+
+	int a;
+
+
+	printf("enter the a");
+	scanf("%d",&a);
+
+
+
+	if(a>0)
+	{
+		printf("a(%d) is positive number",a);
+	}
+	else
+	{
+		printf("a(%d) is nagitive number",a);
+	}
+	return 0;
+}
