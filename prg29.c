89e2c70e232f3270a35f16ea09c97ee54d0d6687
variya utsav <utsavvariya02@gmail.com>
2024-09-02 15:55:49 +0530

    voting

---
+++ prg29.c
@@ -0,0 +1,18 @@
+// voting
+
+#include<stdio.h>
+int main()
+{
+ 	int a;
+	printf("enter the a:");
+	scanf("%d",&a);
+	if(a>=18)
+	{
+	printf(" voting %d",a);
+	}
+	else
+	{
+	printf(" not voting %d",a);
+	}
+	return 0;
+}
