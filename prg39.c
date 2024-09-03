5d19cd9f05a625f5d411bb04ad4f0746a9fbd0cc
variya utsav <utsavvariya02@gmail.com>
2024-09-03 20:57:42 +0530

    cherector

---
+++ prg39.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main ()
+ {
+    char ch='A',ch1='Z',ch2='a';
+    int ch3=122;
+    printf("\n chr: %c - values :%d",ch,ch);
+    printf("\n chr %c - values :%d",ch1,ch1);
+    printf("\n che %c -values : %d",ch2,ch2);
+    printf("\n che %c - values :%d", ch3,ch3);
+
+    return 0;
+}
+
+
