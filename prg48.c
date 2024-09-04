e04c165c8da3249ba45e6830022b9f030370595c
variya utsav <utsavvariya02@gmail.com>
2024-09-04 16:00:17 +0530

    odd print

---
+++ prg48.c
@@ -0,0 +1,15 @@
+//odd print
+
+#include<stdio.h>
+int main()
+{
+    int i;
+    for(i=0;i<=50;i++)
+    {
+        if(i%2==1)
+        {
+            printf(" \n  %d ",i);
+        }
+    }
+    return 0;
+}
