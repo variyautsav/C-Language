c3fc040a5ee04e90c1bfd81a9666bd2da936a824
variya utsav <utsavvariya02@gmail.com>
2024-09-04 15:58:38 +0530

    1_n print

---
+++ prg46.c
@@ -0,0 +1,14 @@
+// 1_n print
+#include<stdio.h>
+int main()
+{
+    int i,n;
+    printf("entre the n");
+    scanf(" %d",&n);
+    for(i=0;i<=n;i++)
+    {
+        printf(" \n %d",i);
+    }
+    return 0;
+
+}
