77016323f19049d468565784c22cab2bd7ec5f01
variya utsav <utsavvariya02@gmail.com>
2024-09-06 15:55:56 +0530

    n_1 peint using for

---
+++ prg52.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int n,i;
+    printf(" enter the  n :");
+    scanf("%d",&n);
+    for(i=n;i>=1;i--)
+    {
+        printf("%d",i);
+
+    }
+    return 0;
+
+}
+
