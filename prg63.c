828058e998ab1188994c77df16cb58904fa3f6ee
variya utsav <utsavvariya02@gmail.com>
2024-09-08 11:40:46 +0530

    0.5,1,2.5,5

---
+++ prg63.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+     float i,n=0.5;
+
+     printf(" %f",n);
+     for(i=0.5;i<=10;i++)
+     {
+      n=n+i;
+      printf(" %f",n);
+     }
+
+     return 0;
+}
