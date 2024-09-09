0283d72195e83f85df4546100ebf9c7642987e4c
variya utsav <utsavvariya02@gmail.com>
2024-09-09 15:57:57 +0530

    ABC petan

---
+++ prg69.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int i,j;
+    for(i=65;i<69;i++)
+    {
+        for(j=65;j<69;j++)
+        {
+            printf("%c \t",i);
+        }
+
+        printf("\n");
+    }
+    return 0;
+}
+/*
+AAAA
+BBBB
+CCCC
+DDDD
+*/
