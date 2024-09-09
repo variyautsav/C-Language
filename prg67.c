92471211668e1e8ab2a75fe6ae61e7d098c7cde9
variya utsav <utsavvariya02@gmail.com>
2024-09-09 15:56:53 +0530

    number petan

---
+++ prg67.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int i,j;
+    for(i=5;i>=1;i--)
+    {
+        for(j=5;j>=1;j--)
+        {
+            printf("%d \t",i);
+        }
+
+        printf("\n");
+    }
+    return 0;
+}
+/* 5555
+4444
+3333
+2222
+1111*/
