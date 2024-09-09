d9f41133e680f398db03c329e88fe251c7b7bc4d
variya utsav <utsavvariya02@gmail.com>
2024-09-09 15:55:34 +0530

    * petan

---
+++ prg65.c
@@ -0,0 +1,21 @@
+/*
+  ***
+  ***
+  ***
+  ***
+
+*/
+#include<stdio.h>
+int main()
+{
+    int i,j;
+    for(i=1;i<=4;i++)//row
+    {
+        for(j=1;j<=3;j++)// colme
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
