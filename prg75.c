863112521a4bec6098d422e29f58f2e75a797dfc
variya utsav <utsavvariya02@gmail.com>
2024-09-10 15:50:19 +0530

    number petan

--- prg75.c
+++ prg75.c
@@ -33,3 +33,78 @@ int main()
 
 
 */
+#include<stdio.h>
+int main()
+{
+    int i,j;
+    for(i=1;i<=5;i++)
+    {
+        for(j=1;j<=i;j++)
+        {
+            printf("*");
+
+        }
+        printf("\n");
+    }
+    return 0;
+}
+// *
+// **
+// ***
+// ****
+// *****
+// ******
+
+
+#include<stdio.h>
+int main()
+{
+    int i,j;
+    for(i=1;i<=5;i++)
+    {
+        for(j=5;j>=i;j--)
+        {
+            printf("*");
+
+        }
+        printf("\n");
+    }
+    return 0;
+}
+// *******
+// *****
+// ****
+// ***
+// **
+// *
+
+
+
+#include<stdio.h>
+int main()
+{
+    int i,j,s=10;
+    for(i=1;i<=5;i++)
+    {
+        for(j=1;j<=s;j++)
+
+
+            printf("  ");
+
+        for(j=1;j<=i;j++)
+        {
+            printf("  *");
+        }
+        s--;
+
+
+
+        printf("\n");
+
+    }
+    return 0;
+}
+
+//    *
+//   * *
+// * * * *
