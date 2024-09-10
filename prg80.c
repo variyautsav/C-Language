27e9c831dcdab522cbdf876118e06a666b457799
variya utsav <utsavvariya02@gmail.com>
2024-09-10 15:57:46 +0530

    pattern

---
+++ prg80.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main()
+{
+    int i,j;
+    for(i=5;i>=1;i--)
+    {
+        for(j=5;j>=i;j--)
+        {
+            printf("%d",j);
+        }
+        printf("\n");
+    }
+    return 0;
+}
+5
+// 54
+// 543
+// 5432
+// 54321
