5ab4447a1267be2fccea98ce9d8da1602cecc2e4
variya utsav <utsavvariya02@gmail.com>
2024-09-13 15:52:22 +0530

    while loop

---
+++ prg88.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+int main()
+{
+    int i,cnt=0;
+    i=1;
+    for(;i<=5;cnt++)
+    {
+        printf("%d", i);
+        i++;
+
+    }
+    printf("cnt %d",cnt);
+
+
+ // while
+// i=0;
+ //while (i<=5)
+ //{
+    //printf("%d",i);
+    //i++;
+    //printf("%d",i++);
+
+ //}
+
+ // do while
+ //i=1;
+ //do
+// {
+  //  printf("%d",i);
+ //   i++;
+// }
+ //while(i<=5);
+
+return 0;
+}
---
+++ prg89.c
@@ -0,0 +1,13 @@
+//  1......10
+#include<stdio.h>
+int main()
+{
+    int i;
+    i=1;
+    while(i<=10)
+    {
+        printf("%d \n ",i++);
+
+    }
+    return 0;
+}
