7cf92ac862b24f349a8773e713775eac4bf831d6
variya utsav <utsavvariya02@gmail.com>
2024-09-14 19:13:28 +0530

    while

---
+++ prg96.c
@@ -0,0 +1,15 @@
+// n to 1
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i,n;
+    cout<<" entre the n :";
+    cin>>n;
+    i=n;
+    while(i>=1)
+    {
+        cout<<i--;
+    }
+    return 0;
+}
