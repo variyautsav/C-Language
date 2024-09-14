6cfad7ae6e29a4295b197ead251c5f2d82810364
variya utsav <utsavvariya02@gmail.com>
2024-09-14 19:14:53 +0530

    while

---
+++ prg100.c
@@ -0,0 +1,16 @@
+// while table
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i,n,total;
+    cout<<"entre the n:";
+    cin>>n;
+    i=1;
+    while(i<=10)
+    {
+        cout<<i<<" * "<< n <<"="<< i*n  <<"\n ";
+        i++;
+    }
+    return 0;
+}
