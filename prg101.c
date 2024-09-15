af49cf8bbc87eb831cacbb43ac51cea75d9d727b
variya utsav <utsavvariya02@gmail.com>
2024-09-15 09:02:34 +0530

    NRNP_function

---
+++ prg101.c
@@ -0,0 +1,21 @@
+//NRNP:-no return no peramiter
+#include<iostream>
+using namespace std;
+ void sum()
+ {
+    int a,b,c;
+    cout<<"entre the a:";
+    cin>>a;
+    cout<<"entre the b:";
+    cin>>b;
+
+    c=a+b;
+
+    cout<<"sum"<<c;
+
+ }
+ int main()
+ {
+    sum();
+    return 0;
+ }
