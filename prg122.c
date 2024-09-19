56814a9dea69df9d52081cc11a3e0f04ea5a93cb
variya utsav <utsavvariya02@gmail.com>
2024-09-19 16:17:24 +0530

    do_while

---
+++ prg122.c
@@ -0,0 +1,30 @@
+/*
+1
+22
+333
+4444
+55555
+*/
+#include<stdio.h>
+int main()
+{
+    int i,j;
+    i=1;
+    do
+    {
+        j=1;
+        do
+        {
+            printf("%d",j);
+            j++;
+        }while(j<=i);
+
+        i++;
+        printf("\n");
+
+
+    }while(i<=5);
+    return 0;
+
+
+}
