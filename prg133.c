7a3ba0ba955245b94b64fd6ccdaa63310622b1f4
variya utsav <utsavvariya02@gmail.com>
2024-09-30 16:13:34 +0530

    while

---
+++ prg133.c
@@ -0,0 +1,26 @@
+/*
+12345
+12345
+12345
+12345
+*/
+#include<stdio.h>
+int main()
+{
+    int i,j;
+    i=1;
+    while(i<=5)
+    {
+        j=1;
+        while(j<=5)
+        {
+            printf("%d",j);
+            j++;
+        }
+        i++;
+        printf("\n");
+
+    }
+    return 0;
+
+}
