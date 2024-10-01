a94ad61adb048c7f8453f8d5bcd881afb1ac6fa0
variya utsav <utsavvariya02@gmail.com>
2024-10-01 18:03:40 +0530

    foor loop print

---
+++ prg140.c
@@ -0,0 +1,35 @@
+/*
+
+ * * * * *
+ *       *
+ *       *
+ * * * * *
+
+
+
+
+
+*/
+
+#include<stdio.h>
+int main()
+{
+    int i,j;
+    for(i=1;i<=5;i++)
+     {
+        for(j=1;j<=5;j++)
+        {
+            if(i==j || i==1 || j==1 || j==1)
+            {
+                printf("*");
+            }
+            else
+            {
+                printf("  ");
+            }
+        }
+        printf("\n");
+     }
+     return 0;
+}
+
