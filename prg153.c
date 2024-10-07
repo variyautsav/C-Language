69ec8a244e686106cf930e7719473f79014a16fe
variya utsav <utsavvariya02@gmail.com>
2024-10-07 15:41:55 +0530

    towdaymentional_array

---
+++ prg153.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+int main()
+{
+    int a[3][3],b[3][3],i,j;
+
+     for(i=0;i<3;i++)
+        {
+            for(j=0;j<3;j++)
+            {
+                printf(" entre the element of arr a :");
+                scanf("%d",&a[i][j]);
+            }
+        }
+
+
+
+    for(i=0;i<3;i++)
+        {
+            for(j=0;j<3;j++)
+            {
+                printf(" entre the element of arr b:");
+                scanf("%d",&b[i][j]);
+            }
+        }
+
+
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            printf("\t %d",a[i][j]);
+        }
+        for(j=0;j<3;j++)
+        {
+            printf("\t %d",b[i][j]);
+        }
+    printf("\n");
+    }
+
+    return 0;
+}
