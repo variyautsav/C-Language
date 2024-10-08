3093a69b89e2eb0e5ea92261ef2987c0e29f7ecb
variya utsav <utsavvariya02@gmail.com>
2024-10-08 15:35:43 +0530

    char

---
+++ prg156.c
@@ -0,0 +1,26 @@
+//char'H','e','l','l','o'
+// string "Hello
+
+
+#include<stdio.h>
+int main()
+{
+ //   char str[]={"utsav"};      // size is not decler initilaigetion
+   // printf("\n %s", str);
+
+   char str1[15]; // size is compelsary dicler
+   printf("Enter the string");
+
+
+  // scanf("%s",str1);// spece affter not print the string
+  // scanf("%[^\n]",str1); // give the space in string to print aaftre the string
+  // printf("%s",str1);
+
+  gets(str1); //  only used in string // gest is functiomn of string // uesd like scanf // give the input of program
+  puts(str1); // only udes in string // puts is function of string // used like printf // printf the statment of program
+
+     return 0;
+}
+
+
+
