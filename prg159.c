124d41a5c0ae1ce27318f666f5c255194cfbe58b
variya utsav <utsavvariya02@gmail.com>
2024-10-08 15:36:36 +0530

    copy  string

---
+++ prg159.c
@@ -0,0 +1,21 @@
+// copy the string
+#include<stdio.h>
+int main()
+{
+
+ char aar[10],aar1[10];
+    int i,k;
+
+        printf("enter the string aar: ");
+        scanf("%s",aar);
+
+
+
+        for(i=0;i<=aar[i]!='\0';i++);
+        for(k=0;k<=i;k++)
+       {
+        aar1[k]=aar[k];
+       }
+       printf("%s",aar1);
+
+}
