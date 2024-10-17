543374e63a94cd02af45d9c018ed714624fdeef5
variya utsav <utsavvariya02@gmail.com>
2024-10-17 17:31:02 +0530

    noreturn no argunment

---
+++ prg176.c
@@ -0,0 +1,20 @@
+// simpe inteset
+// noreturn no argunment
+#include<stdio.h>
+void INTREST()
+{
+    int a,ans;
+    printf("entre the a");
+    scanf("%d",&a);
+    ans = a * 0.1;
+    printf("total intreset %d",ans);
+
+}
+  int main()
+  {
+    void INTREST();
+    INTREST();
+
+    return 0;
+
+  }
