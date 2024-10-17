7058fa723ad318b479a57aff6cf81db2a990ae73
variya utsav <utsavvariya02@gmail.com>
2024-10-17 17:32:01 +0530

    no return  but agrgunmant

---
+++ prg177.c
@@ -0,0 +1,18 @@
+// simpe intrest
+// no return  but agrgunmant
+
+#include<stdio.h>
+void intest(int p)
+{
+    printf(" intest =%f", p*0.1);
+}
+int  main()
+{
+    int p;
+    printf("ntre the p");
+    scanf("%d",&p);
+    void intrest(int);
+    intest(p);
+    return 0;
+
+}
