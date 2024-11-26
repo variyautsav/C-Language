a67c507c4d53bb2e44db054ec2df885ef6ccddc4
variya utsav <utsavvariya02@gmail.com>
2024-11-26 14:55:11 +0530

    no return no perameter

---
+++ prg182.c
@@ -0,0 +1,20 @@
+// no return no perameter
+// square
+#include<stdio.h>
+ void square()
+ {
+    int l,square;
+    printf("entre the lenght");
+    scanf("%d",&l);
+    square=l*l;
+    printf("%d",square);
+
+
+
+ }
+ int  main()
+ {
+    square();
+    return 0;
+
+ }
