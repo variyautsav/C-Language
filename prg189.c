778b7ebc98afcce01cd9f9fe8de9dd41ed4279f4
variya utsav <utsavvariya02@gmail.com>
2024-11-26 14:58:39 +0530

    // with perameter no return // squear

---
+++ prg189.c
@@ -0,0 +1,17 @@
+// with perameter no return
+// squear
+#include<stdio.h>
+float square(float l)
+{
+    printf("%f",l*l);
+}
+int main()
+{
+    float l;
+    float square(float);
+    printf("entre the lanth");
+    scanf("%f",&l);
+
+    square(l);
+    return 0;
+}
