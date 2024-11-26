816cfb0ec23368ce83ad368bb66916fad90babd2
variya utsav <utsavvariya02@gmail.com>
2024-11-26 14:59:11 +0530

    // with peremeter no return // rectengel

---
+++ prg190.c
@@ -0,0 +1,25 @@
+// with peremeter no return
+// rectengel
+#include<stdio.h>
+
+void area(float h,float b)
+{
+
+    printf(" : %f",0.5*h*b);
+}
+
+int main()
+{
+
+      float h,b;
+
+    void area(float,float);
+     printf("enter the height");
+    scanf("%f",&h);
+
+     printf("enter the width");
+    scanf("%f",&b);
+
+    area(h,b);
+    return 0;
+}
