24831b343976ea012f3c53ff3a8a07ae5adab93e
variya utsav <utsavvariya02@gmail.com>
2024-11-26 14:57:34 +0530

    // with return with peramiter // rectengel

---
+++ prg186.c
@@ -0,0 +1,20 @@
+// with return with peramiter
+// rectengel
+#include<stdio.h>
+float rect(float l,float b)
+{
+    return l*b;
+
+}
+int main()
+{
+    float l,b;
+    float rect(float,float);
+    printf("entre the lanth");
+    scanf("%f",&l);
+    printf("entre the bridth");
+    scanf("%f",&b);
+    printf(" %f",rect(l,b));
+    return 0;
+
+}
