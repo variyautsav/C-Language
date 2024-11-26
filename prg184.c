e701b6ba841bad6ec29fb916acf2846b964b4799
variya utsav <utsavvariya02@gmail.com>
2024-11-26 14:56:24 +0530

    // with permeter with return ; // circale

---
+++ prg184.c
@@ -0,0 +1,16 @@
+// with permeter with return ;
+// circale
+#include<stdio.h>
+float ciracle(float r)
+{
+    return 3.14*r*r;
+}
+int main()
+{
+    float r;
+    float ciracle(float);
+    printf("entre the r:");
+    scanf("%f",&r);
+ printf("sum =%f",ciracle(r));
+    return 0;
+}
