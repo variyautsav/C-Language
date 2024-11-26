4af821253433fce534b0c538a7cceeca8523fb14
variya utsav <utsavvariya02@gmail.com>
2024-11-26 14:56:48 +0530

    // with return with perameter //square

---
+++ prg185.c
@@ -0,0 +1,22 @@
+// with return with perameter
+//square
+#include<stdio.h>
+float square(float l)
+{
+    return l*l;
+
+}
+int main()
+{
+    float l ;
+    float  square(float);
+
+    printf("entre the lenght");
+    scanf("%f",&l);
+
+    printf("%f",square(l));
+    return 0;
+
+
+}
+
