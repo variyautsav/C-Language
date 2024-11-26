b511c531bce70054fa03598e4435b082dab8ea9a
variya utsav <utsavvariya02@gmail.com>
2024-11-26 14:55:52 +0530

    // no retuen no peramiter //rectemngel

---
+++ prg183.c
@@ -0,0 +1,21 @@
+// no retuen no peramiter
+//rectemngel
+#include<stdio.h>
+int rect()
+{
+    int l,b,rect;
+    printf("entre the lanth");
+    scanf("%d",&l);
+    printf("enter the bridth" );
+    scanf("%d",&b);
+
+    rect=l*b;
+    printf("%d",rect);
+}
+int main()
+{
+    rect();
+    return 0;
+
+}
+    
