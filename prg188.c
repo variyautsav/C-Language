4a44ef77ec16b4c2fbda665db794866185033b40
variya utsav <utsavvariya02@gmail.com>
2024-11-26 14:58:17 +0530

    // with peramiter no return //circal

---
+++ prg188.c
@@ -0,0 +1,18 @@
+// with peramiter no return
+//circal
+#include<stdio.h>
+float ciracl(float r)
+{
+    printf("%f= " , 3.14*r*r);
+}
+int main()
+{
+    float r ;
+    float  ciracl(float);
+    printf("entre the r");
+    scanf("%f",&r);
+
+     ciracl(r);
+    return 0;
+
+}
