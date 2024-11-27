16b58c1fe1b8eb331fb9fb3ee833c871c959446e
variya utsav <utsavvariya02@gmail.com>
2024-11-27 15:30:10 +0530

    // with pera no return // trengel

---
+++ prg191.c
@@ -0,0 +1,20 @@
+// with pera no return
+// trengel
+#include<stdio.h>
+float trengel(float b, float h)
+{
+    printf("%f",0.5*b*h);
+
+}
+int main()
+{
+    float b ,h ;
+    float trengel(float,float);
+    printf ("entre  the b ");
+    scanf("%f",&b);
+
+    printf("entre the h: ");
+    scanf("%f",&h);
+    trengel(b,h);
+    return 0;
+}
