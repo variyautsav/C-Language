15b41e321fec0dec19e52e407bf25c06a413d3e0
variya utsav <utsavvariya02@gmail.com>
2024-11-27 15:30:30 +0530

    // with return no pera //circal

---
+++ prg192.c
@@ -0,0 +1,19 @@
+// with return no pera
+//circal
+#include<stdio.h>
+float ciracl()
+{  float r ;
+
+    printf("entre the r");
+    scanf("%f",&r);
+
+    return  3.14*r*r;
+}
+int main()
+{
+     float  ciracl();
+
+     printf("%f",ciracl());
+    return 0;
+
+}
